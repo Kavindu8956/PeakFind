5dae003834b6e7eb77b0fc3d0a11d359ea5b0405
Kavindu8956 <sandeepa8956@gmail.com>
2019-08-04 06:51:53 +0530

    Kavindu_edit2

--- app/src/main/java/com/example/peakfind/MainActivity.java
+++ app/src/main/java/com/example/peakfind/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.peakfind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hotel(View v) {
+        Intent intentHotel = new Intent(MainActivity.this,HotelActivity.class);
+        startActivity(intentHotel);
+    }
+
+    public void cab(View v) {
+        Intent intentCab = new Intent(MainActivity.this,CabActivity.class);
+        startActivity(intentCab);
+    }
+    public void shoppingmall(View v) {
+        Intent intentShoppingMall = new Intent(MainActivity.this,ShoppingMallActivity.class);
+        startActivity(intentShoppingMall);
+    }
+    public void weather(View v) {
+        Intent intentWeather = new Intent(MainActivity.this,WeatherActivity.class);
+        startActivity(intentWeather);
     }
 }

7bf2b22a8a7366d5f6e9ddc50104c147da8af3c8
Kavindu8956 <sandeepa8956@gmail.com>
2019-08-04 17:30:32 +0530

    Kavindu_edit4

---
+++ app/src/main/java/com/example/peakfind/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.peakfind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+   private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button=(Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

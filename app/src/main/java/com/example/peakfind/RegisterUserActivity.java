f2be111655f3f1dce486e47bf6f51bd6c41da628
Kavindu8956 <sandeepa8956@gmail.com>
2019-09-13 11:54:49 +0530

    Kavindu_edit18(User registration in Firebase)

--- app/src/main/java/com/example/peakfind/RegisterUserActivity.java
+++ app/src/main/java/com/example/peakfind/RegisterUserActivity.java
@@ -1,20 +1,40 @@
 package com.example.peakfind;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUserActivity extends AppCompatActivity implements View.OnClickListener{
 
+    EditText editTextEmail,editTextPassword;
+    ProgressBar progressBar;
 
-    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeruser);
 
+        editTextEmail = (EditText) findViewById(R.id.txtEmail);
+        editTextPassword = (EditText) findViewById(R.id.txtPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.btnRegister).setOnClickListener(this);
 
     }
 
@@ -33,5 +53,45 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
     private void registerUser() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum lenght of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),"User Registered Successfull", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterUserActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),"User Registration Unsuccessfull", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

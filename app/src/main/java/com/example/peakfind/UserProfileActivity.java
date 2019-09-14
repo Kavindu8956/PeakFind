78908aab69e16dd1c4c18638f52af69f7e41390f
Kavindu8956 <sandeepa8956@gmail.com>
2019-09-14 16:01:54 +0530

    Kavindu_edit21(Insert)

--- app/src/main/java/com/example/peakfind/UserProfileActivity.java
+++ app/src/main/java/com/example/peakfind/UserProfileActivity.java
@@ -1,52 +1,18 @@
 package com.example.peakfind;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-
-import java.io.IOException;
 
 public class UserProfileActivity extends AppCompatActivity {
-
-    private static final int CHOOSE_IMAGE = 101;
-
-    ImageView imageView;
-    EditText editText;
-
-    Uri uriProfileImage;
 
     private Button b1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-
-        editText = (EditText) findViewById(R.id.editTextName);
-        imageView = (ImageView) findViewById(R.id.imageViewProfile);
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showImageChooser();
-
-            }
-        });
-
-        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-        /*
         b1=(Button)findViewById(R.id.button9) ;
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,32 +21,7 @@ public class UserProfileActivity extends AppCompatActivity {
                 startActivity(i1);
             }
         });
-            //   @Override*/
+            //   @Override
 
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            uriProfileImage = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriProfileImage);
-                imageView.setImageBitmap(bitmap);
-
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void showImageChooser() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
     }
 }
